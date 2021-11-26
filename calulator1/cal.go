b8ace23590bd74ecd03c13e36873dfc977ce20c2
codecoco <88923210+codecocos@users.noreply.github.com>
2021-11-26 16:03:45 +0900

    go 기초 내용

---
+++ calulator1/cal.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	var b int
+	//변수는 메모리 번지를 가지고 있다.
+	//메모리 번지 : 메모리상 어떤 부분에 값이 작성될 것이다.
+	a = 3
+	b = 4
+
+	fmt.Println(a+b)
+}
---
+++ hello/hello.go
@@ -0,0 +1,12 @@
+//시작점
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello 월드")
+}
+
+// func main(입력) 출력 {시작점
+	
+// }
