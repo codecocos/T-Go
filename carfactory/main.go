fcc3b3877d42c0500571289119fa5ab9b46bc4b1
codecoco <88923210+codecocos@users.noreply.github.com>
2021-12-06 23:39:36 +0900

    channel 예시

---
+++ carfactory/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+type Car struct {
+	val string
+}
+
+func MakeTire(carChan chan Car, outChan chan Car) {
+  for{	
+		car := <-carChan
+		car.val += "Tire, "
+
+		outChan <- car
+	}
+
+}
+
+func MakeEngine(carChan chan Car, outChan chan Car) {
+	
+	for {
+		car := <-carChan
+		car.val += "Engine, "
+
+		outChan <- car
+	}
+}
+
+func StartWork(chan1 chan Car)  {
+	i := 0
+	for {
+		time.Sleep(1 * time.Second)
+		//strconv.Itoa(i) : 문자를 숫자로 바꿔줌.
+		chan1 <- Car{val: "Car" + strconv.Itoa(i) }
+		i++
+	}
+}
+
+func main() {
+	chan1 := make(chan Car)
+	chan2 := make(chan Car)
+	chan3 := make(chan Car)
+
+	go StartWork(chan1)
+	go MakeTire(chan1, chan2)
+	go MakeEngine(chan2, chan3)
+
+	for{
+	result := <-chan3
+	fmt.Println(result.val)
+	}
+
+}
+
+/*
+main    -    makeTire    -    makeEngine
+car1- chan1 -  tire  - chan2  - engine
+*/
